2efb7599fc9fd2421105f62c90783d353e8b23b4
ndominkiewicz <norbertd1410@gmail.com>
2025-10-27 03:28:31 +0100

    desktop: injection of a newProject variable and mainController, also added setter for MainController and initialize enables mouse event on newProject Pane to switch the view

--- desktop/src/main/java/com/sheetlab/desktop/controllers/DashboardController.java
+++ desktop/src/main/java/com/sheetlab/desktop/controllers/DashboardController.java
@@ -1,15 +1,35 @@
 package com.sheetlab.desktop.controllers;
 
 import com.sheetlab.desktop.models.ViewController;
+import com.sheetlab.desktop.utils.Page;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 
-public class DashboardController implements ViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements ViewController, Initializable {
+    MainController mainController;
+    @FXML
+    private GridPane newProject;
     @FXML
     private GridPane root;
     @Override
     public Node getView() {
         return root;
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        newProject.setOnMousePressed(event -> {
+            if (mainController != null) {
+                mainController.switchView(Page.SETUP);
+            }
+        });
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
     }
 }
